f6a0a81e12e4418b2a80aa2da724c4b5bb544452
Juan Cruz Muñoz <juanchomc17@gmail.com>
2020-11-03 20:34:49 -0300

    Fin clase 2

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -7,6 +7,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductListComponent implements OnInit {
 
+  product = {
+    "name": "Nike Protector",
+    "type": "Rodillera",
+    "price": 500,
+    "stock": 20,
+    "image":"assets/img/rodillera-nike.jpg"
+  }
+  product2 = {
+    "name": "Adidas Protector",
+    "type": "Manga",
+    "price": 300,
+    "stock": 25
+  }
+  product3 = {
+    "name": "Topper Protector",
+    "type": "Rodillera",
+    "price": 400,
+    "stock": 30
+  }
+  product4 = {
+    "name": "Mikasa Protector",
+    "type": "Manga",
+    "price": 700,
+    "stock": 10
+  }
   constructor() { }
 
   ngOnInit(): void {
