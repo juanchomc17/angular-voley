1d0b7153a86f68dc45b9fea4a679e24617470e34
Juan Cruz Muñoz <juanchomc17@gmail.com>
2020-11-04 21:25:55 -0300

    Termine todo de la 3er clase

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Product } from './product';
 
 @Component({
   selector: 'app-product-list',
@@ -7,34 +8,40 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductListComponent implements OnInit {
 
-  product = {
-    "name": "Nike Protector",
-    "type": "Rodillera",
-    "price": 500,
-    "stock": 20,
-    "image":"assets/img/rodillera-nike.jpg"
-  }
-  product2 = {
-    "name": "Adidas Protector",
-    "type": "Manga",
-    "price": 300,
-    "stock": 25,
-    "image":"assets/img/manga-adidas.jpg"
-  }
-  product3 = {
-    "name": "DRB Protector",
-    "type": "Rodillera",
-    "price": 400,
-    "stock": 30,
-    "image":"assets/img/rodillera-drb.jpg"
-  }
-  product4 = {
-    "name": "Mikasa Protector",
-    "type": "Manga",
-    "price": 700,
-    "stock": 10,
-    "image":"assets/img/manga-mikasa.jpg"
-  }
+  products: Product[] = [
+    {
+      "name": "Nike Protector",
+      "type": "Rodillera",
+      "price": 500,
+      "stock": 0,
+      "image":"assets/img/rodillera-nike.jpg",
+      "clearance": false,
+    },
+    {
+      "name": "Adidas Protector",
+      "type": "Manga",
+      "price": 300,
+      "stock": 25,
+      "image":"assets/img/manga-adidas.jpg",
+      "clearance": true,
+    },
+    {
+      "name": "DRB Protector",
+      "type": "Rodillera",
+      "price": 400,
+      "stock": 30,
+      "image":"assets/img/rodillera-drb.jpg",
+      "clearance": false,
+    },
+    {
+      "name": "Mikasa Protector",
+      "type": "Manga",
+      "price": 700,
+      "stock": 10,
+      "image":"assets/img/manga-mikasa.jpg",
+      "clearance": false,
+    },
+  ];
   constructor() { }
 
   ngOnInit(): void {
